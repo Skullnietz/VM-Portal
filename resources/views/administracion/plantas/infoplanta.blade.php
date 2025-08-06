12594018acbb0ec9d7572e90a3de775ac1c60eb0
Carlos Guizar <carlos.guizar@urvina.com.mx>
2025-08-06 16:00:50 -0600

    Semi Agent

--- resources/views/administracion/plantas/infoplanta.blade.php
+++ resources/views/administracion/plantas/infoplanta.blade.php
@@ -1258,6 +1258,7 @@ $('#areas,#empleados').on('click', '.btn-info', function(e) {
                     data: function (d) {
                         d._token = '{{ csrf_token() }}';
                     },
+                }
             columns: [
                 { data: 'No_Empleado', name: 'No_Empleado' },
                 { data: 'Nip', name: 'Nip' },
@@ -1322,7 +1323,7 @@ $('#areas,#empleados').on('click', '.btn-info', function(e) {
                 sortDescending: ": activar para ordenar la columna de manera descendente"
             }
         }
-    }
+    
     });
 
 
