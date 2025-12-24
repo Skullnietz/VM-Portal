970ff1de528ca17e9d9e9d8d5eb72bad24b48784
Carlos Guizar <carlos.guizar@urvina.com.mx>
2025-12-24 10:26:48 -0600

    Fix AppServiceProvider artisan error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,9 @@ class AppServiceProvider extends ServiceProvider
         if (!file_exists($path)) {
             @mkdir($path, 0755, true);
         }
-        ini_set('session.save_path', $path);
-        ini_set('session.gc_probability', 0);
+        if (!app()->runningInConsole()) {
+            ini_set('session.save_path', $path);
+            ini_set('session.gc_probability', 0);
+        }
     }
 }
